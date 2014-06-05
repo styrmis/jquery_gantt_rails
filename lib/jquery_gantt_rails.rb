507e22255fe171b3707625af057055d5dd18ea9e
Stefan Haflidason <styrmis@gmail.com>
2014-06-05 12:37:35 +0100

    Fixed inclusion of image assets

--- lib/jquery_gantt_rails.rb
+++ lib/jquery_gantt_rails.rb
@@ -1,8 +1,8 @@
 module JqueryGanttRails
   class Engine < Rails::Engine
     initializer :assets do |config|
-      Rails.application.config.assets.precompile += %w{ jquery.gantt.css }
-      Rails.application.config.assets.precompile += %w{ jquery.gantt.js }
+      Rails.application.config.assets.precompile += %w{ jquery.gantt.css jquery.gantt.js }
+      Rails.application.config.assets.precompile += %w{ jquery-gantt/grid.png jquery-gantt/icon_sprite.png jquery-gantt/loader_bg.png jquery-gantt/slider_handle.png }
       Rails.application.config.assets.paths << root.join("vendor", "assets", "images")
     end
   end
--- lib/jquery_gantt_rails/version.rb
+++ lib/jquery_gantt_rails/version.rb
@@ -1,5 +1,5 @@
 module JqueryGanttRails
   # Based on master branch on 4/7/2014
   # https://github.com/mbielanczuk/jQuery.Gantt
-  VERSION = "0.0.1"
+  VERSION = "0.0.2"
 end
